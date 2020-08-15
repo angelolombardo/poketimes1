2b4e3c6b0a2115e6a0d747116c43a3f6a1883f63
angelolombardo <alombardo1030@gmail.com>
2020-08-15 15:53:19 -0400

    Updates

---
+++ precache-manifest.5763f796f46a242d5c98acd67fab2758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6419b82782f6d099d6b0c3aec341ae86",
+    "url": "/poketimes1/index.html"
+  },
+  {
+    "revision": "0e2bd211ccd59bbb0d06",
+    "url": "/poketimes1/static/css/main.bb55d663.chunk.css"
+  },
+  {
+    "revision": "654b919b8cce2772007d",
+    "url": "/poketimes1/static/js/2.db2166dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/poketimes1/static/js/2.db2166dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2bd211ccd59bbb0d06",
+    "url": "/poketimes1/static/js/main.f51a3eb0.chunk.js"
+  },
+  {
+    "revision": "3cf7d40bb68e03538141",
+    "url": "/poketimes1/static/js/runtime-main.b5eb058b.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/poketimes1/static/media/pokeball.f10bf078.png"
+  }
+]);
